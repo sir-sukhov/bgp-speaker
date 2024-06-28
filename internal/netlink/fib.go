2fb0903a02e0b3fbf8b6a346a9a0afaf210cf8c8
Ilya Sukhov <sukhov@phystech.edu>
2024-06-28 13:26:44 +0300

    Multipath route setup example

--- internal/netlink/fib.go
+++ internal/netlink/fib.go
@@ -1,9 +1,12 @@
 package netlink
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"strings"
 
+	"github.com/jsimonetti/rtnetlink"
 	"github.com/jsimonetti/rtnetlink/rtnl"
 )
 
@@ -32,7 +35,7 @@ func PrintRoutes() error {
 		ifindex := int(rt.Attributes.OutIface)
 		iface, ok := linksMap[ifindex]
 		if !ok {
-			return fmt.Errorf("failed to get name of interfae with index %d", ifindex)
+			return fmt.Errorf("failed to get name of interface with index %d: might be multipath: %s", ifindex, errors.ErrUnsupported)
 		}
 		var dst string
 		if rt.Attributes.Dst == nil {
@@ -53,6 +56,13 @@ func PrintRoutes() error {
 
 // SetDefaultRoute добавляет или заменяет маршрут по-умолчанию.
 func SetDefaultRoute(gateway string) error {
+	if strings.Contains(gateway, ",") {
+		gwIps := []net.IP{}
+		for _, gwString := range strings.Split(gateway, ",") {
+			gwIps = append(gwIps, net.ParseIP(gwString))
+		}
+		return setMultipathDefaultRoute(gwIps)
+	}
 	c, err := rtnl.Dial(nil)
 	if err != nil {
 		return err
@@ -68,6 +78,50 @@ func SetDefaultRoute(gateway string) error {
 		opts.Attrs.Priority = 50
 	}
 	if err := c.RouteReplace(routeToGw.Interface, *ipNet, gw, withRoutePriority); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Функция setMultipathDefaultRoute добавляет т.н. [multipath route].
+//
+// [multipath route]: https://codecave.cc/multipath-routing-in-linux-part-1.html
+func setMultipathDefaultRoute(gateways []net.IP) error {
+	c, err := rtnl.Dial(nil)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	withRoutePriority := func(opts *rtnl.RouteOptions) {
+		opts.Attrs.Priority = 50
+	}
+	gwInterfaceIndexes := make([]int, len(gateways))
+	for i, gw := range gateways {
+		routeToGw, err := c.RouteGet(gw)
+		if err != nil {
+			return fmt.Errorf("route lookup to %s failed: %w", gw, err)
+		}
+		gwInterfaceIndexes[i] = routeToGw.Interface.Index
+	}
+	withMultipathRoute := func(opts *rtnl.RouteOptions) {
+		nextHops := make([]rtnetlink.NextHop, 0, len(gateways))
+		for i, gw := range gateways {
+			nextHops = append(nextHops, rtnetlink.NextHop{
+				Hop: rtnetlink.RTNextHop{
+					Length:  16,
+					IfIndex: uint32(gwInterfaceIndexes[i]),
+				},
+				Gateway: gw,
+			})
+		}
+		opts.Attrs.Multipath = nextHops
+	}
+	noInterface := &net.Interface{
+		Index: 0,
+	}
+	_, allNets, _ := net.ParseCIDR("0.0.0.0/0")
+	noDst := net.IPv4(0, 0, 0, 0)
+	if err := c.RouteReplace(noInterface, *allNets, noDst, withRoutePriority, withMultipathRoute); err != nil {
 		return err
 	}
 	return nil
